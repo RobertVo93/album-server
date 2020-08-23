dbc6fd5d75a2f0ce05c6f104b09fc028c2163a96
Robert Vo <>
2020-08-23 12:43:31 +0700

    initial code

--- services/index.js
+++ services/index.js
@@ -6,51 +6,51 @@ const { createHash } = require('crypto')
 
 const { uploadFile } = require('./uploader')
 
-async function getFiles (dir) {
-  const children = await readdir(dir, { withFileTypes: true })
-  const files = await Promise.all(children.map((child) => {
-    const res = resolve(dir, child.name)
-    return child.isDirectory() ? getFiles(res) : res
-  }))
+async function getFiles(dir) {
+    const children = await readdir(dir, { withFileTypes: true })
+    const files = await Promise.all(children.map((child) => {
+        const res = resolve(dir, child.name)
+        return child.isDirectory() ? getFiles(res) : res
+    }))
 
-  return Array.prototype.concat(...files)
+    return Array.prototype.concat(...files)
 }
 
-const listFiles = async (host, skip, limit) => {
-  const root = path.join(process.cwd(), 'albums')
-  const files = await getFiles(root)
+const listFiles = async(host, skip, limit) => {
+    const root = path.join(process.cwd(), 'albums')
+    const files = await getFiles(root)
 
-  const documents = Enumerable.from(files)
-    .select(x => {
-      const url = x.replace(root, '')
-      const parts = url.split('/').filter(x => x) || []
+    const documents = Enumerable.from(files)
+        .select(x => {
+            const url = x.replace(root, '')
+            const parts = url.split('/').filter(x => x) || []
 
-      const raw = `${host}/photos/${parts.join('/')}`
-      const name = parts.pop()
-      const album = (parts[0] || '').replace(/\b\S/g, t => t.toUpperCase())
-      const path = `/albums/${album}/${name}`
+            const raw = `${host}/photos/${parts.join('/')}`
+            const name = parts.pop()
+            const album = (parts[0] || '').replace(/\b\S/g, t => t.toUpperCase())
+            const path = `/albums/${album}/${name}`
 
-      return {
-        id: createHash('md5').update(raw).digest('hex'),
-        album,
-        name,
-        path,
-        raw
-      }
-    })
-    .where(x => x.album)
-    .orderByDescending(x => x.id)
-    .skip(skip)
-    .take(limit)
-    .toArray()
+            return {
+                id: createHash('md5').update(raw).digest('hex'),
+                album,
+                name,
+                path,
+                raw
+            }
+        })
+        .where(x => x.album)
+        .orderByDescending(x => x.id)
+        .skip(skip)
+        .take(limit)
+        .toArray()
 
-  return {
-    documents,
-    count: documents.length
-  }
+    return {
+        documents,
+        count: documents.length
+    }
 }
 
 module.exports = {
-  uploadFile,
-  listFiles
+    uploadFile,
+    listFiles
 }
